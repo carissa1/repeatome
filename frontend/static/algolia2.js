e8b4ed043ccb617dedc0ecf0216493cce8391411
Carissa <carissapenn123@gmail.com>
2025-07-10 14:15:18 -0700

    Add algolia searching

--- frontend/static/algolia2.js
+++ frontend/static/algolia2.js
@@ -1,13 +1,13 @@
 // import algoliasearch from 'algoliasearch/lite';
 // import { autocomplete } from '@algolia/autocomplete-js';
 
-window.addEventListener('DOMContentLoaded', () => {
+export default async function initAutocomplete() {
     const searchClient = algoliasearch('I8J408MSCM', '7a38c6848180ee8d3723175b5ec3cc4e');
 
-    const proteinIndex = searchClient.initIndex('proteinTF');
-    const repeatIndex = searchClient.initIndex('repeat');
-    const referenceIndex = searchClient.initIndex('referenc');
-    const organismIndex = searchClient.initIndex('organism');
+    const proteinIndex = searchClient.initIndex('ProteinTF_prod');
+    const repeatIndex = searchClient.initIndex('Repeat_prod');
+    const referenceIndex = searchClient.initIndex('Reference_prod');
+    const organismIndex = searchClient.initIndex('Organism_prod');
 
     console.log('algoliasearch:', typeof algoliasearch);
     console.log('searchClient:', searchClient);
@@ -15,71 +15,77 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('autocomplete:', typeof autocomplete);
 
     autocomplete({
-    container: '#algolia-search-input',
-    placeholder: 'Search a protein or repeat',
-    getSources({ query }) {
-        return [
-        {
-            sourceId: 'proteinTF',
-            getItems() {
-            return proteinIndex.search(query, { hitsPerPage: 5 }).then(res => res.hits);
+        container: '#algolia-search-input', // still required, but will wrap around input
+        detachedMediaQuery: 'none',             // disables fullscreen modal mode
+        placeholder: 'Search a protein or repeat',
+        openOnFocus: true,
+        inputElement: document.querySelector('#algolia-search-input'),
+        getSources({ query }) {
+            return [
+            {
+                sourceId: 'proteinTF',
+                getItems() {
+                    return proteinIndex.search(query, { hitsPerPage: 5 }).then(res => {
+                        console.log('Search results:', res.hits);
+                        return res.hits;
+                    });
+                },
+                templates: {
+                header({ html }) {
+                    return html`<div class="aa-header">Proteins</div>`;
+                },
+                item({ item, html }) {
+                    var str = item.gene;
+                    return html`<a href='${item.url}'><div>${str}</div></a>`;
+                }
+                }
             },
-            templates: {
-            header() {
-                return '<div class="aa-header">Proteins</div>';
+            {
+                sourceId: 'repeat',
+                getItems() {
+                return repeatIndex.search(query, { hitsPerPage: 5 }).then(res => res.hits);
+                },
+                templates: {
+                header({ html }) {5
+                    return html`<div class="aa-header">Repeat</div>`;
+                },
+                item({ item, html }) {
+                    var str = item.name;
+                    return html`<a href='${item.url}'><div>${str}</div></a>`;
+                }
+                }
             },
-            item({ item }) {
-                str = item.gene;
-                return "<a href='" + item.url + "'><div>" + str + "</div></a>";
-            }
-            }
-        },
-        {
-            sourceId: 'repeat',
-            getItems() {
-            return repeatIndex.search(query, { hitsPerPage: 3 }).then(res => res.hits);
+            {
+                sourceId: 'reference',
+                getItems() {
+                return referenceIndex.search(query, { hitsPerPage: 5 }).then(res => res.hits);
+                },
+                templates: {
+                header({ html }) {
+                    return html`<div class="aa-header">Reference</div>`;
+                },
+                item({ item, html }) {
+                    var str = item.title;
+                    return html`<a href='${item.url}'><div>${str}</div></a>`;
+                }
+                }
             },
-            templates: {
-            header() {
-                return '<div class="aa-header">Repeat</div>';
+            {
+                sourceId: 'organism',
+                getItems() {
+                return organismIndex.search(query, { hitsPerPage: 5 }).then(res => res.hits);
+                },
+                templates: {
+                header({ html }) {
+                    return html`<div class="aa-header">Organism</div>`;
+                },
+                item({ item, html }) {
+                    var str = item.scientific_name;
+                    return html `<a href='${item.url}'><div>${str}</div></a>`;
+                }
+                }
             },
-            item({ item }) {
-                str = item.name;
-                return "<a href='" + item.url + "'><div>" + str + "</div></a>";
-            }
-            }
-        },
-        {
-            sourceId: 'reference',
-            getItems() {
-            return referenceIndex.search(query, { hitsPerPage: 2 }).then(res => res.hits);
-            },
-            templates: {
-            header() {
-                return '<div class="aa-header">Reference</div>';
-            },
-            item({ item }) {
-                str = item.title;
-                return "<a href='" + item.url + "'><div>" + str + "</div></a>";
-            }
-            }
-        },
-        {
-            sourceId: 'organism',
-            getItems() {
-            return organismIndex.search(query, { hitsPerPage: 2 }).then(res => res.hits);
-            },
-            templates: {
-            header() {
-                return '<div class="aa-header">Organism</div>';
-            },
-            item({ item }) {
-                str = item.scientific_name;
-                return "<a href='" + item.url + "'><div>" + str + "</div></a>";
-            }
-            }
-        },
-        ];
-    }
+            ];
+        }
     });
-});
+}
